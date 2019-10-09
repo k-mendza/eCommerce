4acad7bf3334a83c6621d17517de00415198bfb0
Karol Mendza <karolmendza@gmail.com>
2019-10-09 22:02:20 +0200

    Added search button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 
-import { MatToolbarModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { MatToolbarModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -19,6 +19,7 @@ import { MatToolbarModule, MatFormFieldModule, MatIconModule, MatInputModule, Ma
     MatInputModule,
     MatFormFieldModule,
     MatIconModule,
+    MatButtonModule,
     MatSelectModule
   ],
   providers: [],
